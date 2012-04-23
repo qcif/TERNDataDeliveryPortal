a285b66e49114183226374ce886ba978148c9e74
Dewi W <root@demo.(none)>
2012-04-23 10:13:11 +1000

    Google Analytics

--- tern-portal/application/views/tpl/header.php
+++ tern-portal/application/views/tpl/header.php
@@ -16,7 +16,7 @@ limitations under the License.
 *
 **/ $md_title = 'TERN Data Discovery Portal (Test Version)';
 	$md_description = 'TERN Data Discovery Portal (Test Version)is a mesh of searchable web pages describing (and where possible linking to) terrestrial ecosystem research data collections. ';
-	$md_image = 'http://services.ands.org.au/home/orca/rda/img/rda-design.png';
+	//$md_image = 'http://services.ands.org.au/home/orca/rda/img/rda-design.png';
 if(isset($title))$md_title = $title .' - TERN Data Discovery Portal';
 	if(isset($description))$md_description = htmlentities($description);
         
@@ -49,16 +49,17 @@ if(isset($title))$md_title = $title .' - TERN Data Discovery Portal';
 
 	<?php if($this->config->item('GA_enabled')):?>
 	<script type="text/javascript">
-           /* 
+            
 	  var _gaq = _gaq || [];
 	  _gaq.push(['_setAccount', '<?php echo $this->config->item('GA_code');?>']);
+            _gaq.push(['_setDomainName', '<?php echo $this->config->item('GA_domain_name');?>']);
 	  _gaq.push(['_trackPageview']);
 	
 	  (function() {
 	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
 	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	  })();*/
+	  })();
 	</script>
                
 	<?php endif;?>
