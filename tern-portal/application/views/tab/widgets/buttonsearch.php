f076b42a6c81ff94f5bc44669613a59957ff4ad5
Yi Sun <sunyi000@gmail.com>
2012-06-13 16:48:07 +1000

    changed button size

--- tern-portal/application/views/tab/widgets/buttonsearch.php
+++ tern-portal/application/views/tab/widgets/buttonsearch.php
@@ -5,11 +5,11 @@
  * and open the template in the editor.
  */
 ?>
-  <div class="buttonSearch" style="float:right;width:126px">
+  <div class="buttonSearch" style="float:right;width:129px">
       <div class="ui-corner-all button-border" style="width:59px;float:left ">
      <button id="search_reset" style="width:59px" >Reset</button>
      </div>
      <div class="ui-corner-all button-border" style="width: 59px;float:right">
-     <button id="search_advanced" >Search</button>
+     <button id="search_advanced" style="width:59px" >Search</button>
      </div>
   </div>
