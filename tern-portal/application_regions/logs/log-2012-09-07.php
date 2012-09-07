b832edf39f4776135717404563028e805568ecf3
Yi Sun <sunyi000@gmail.com>
2012-09-07 14:03:14 +1000

    fix schema.xml error

---
+++ tern-portal/application_regions/logs/log-2012-09-07.php
@@ -0,0 +1,146 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+DEBUG - 2012-09-07 11:55:34 --> Config Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> Hooks Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> Utf8 Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> UTF-8 Support Enabled
+DEBUG - 2012-09-07 11:55:34 --> URI Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> Router Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> Output Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> Security Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> Input Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> Global POST and COOKIE data sanitized
+DEBUG - 2012-09-07 11:55:34 --> Language Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> Loader Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> Helper loaded: url_helper
+DEBUG - 2012-09-07 11:55:34 --> Helper loaded: form_helper
+DEBUG - 2012-09-07 11:55:34 --> Helper loaded: cookie_helper
+DEBUG - 2012-09-07 11:55:34 --> Helper loaded: regions_helper
+DEBUG - 2012-09-07 11:55:34 --> Language file loaded: language/english/rif_cs_lang.php
+DEBUG - 2012-09-07 11:55:34 --> Database Driver Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> cURL Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> Controller Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> Model Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> Model Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> Final output sent to browser
+DEBUG - 2012-09-07 11:55:34 --> Total execution time: 0.1495
+DEBUG - 2012-09-07 11:55:34 --> Config Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> Hooks Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> Utf8 Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> UTF-8 Support Enabled
+DEBUG - 2012-09-07 11:55:34 --> URI Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> Router Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> Output Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> Security Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> Input Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> Global POST and COOKIE data sanitized
+DEBUG - 2012-09-07 11:55:34 --> Language Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> Loader Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> Helper loaded: url_helper
+DEBUG - 2012-09-07 11:55:34 --> Helper loaded: form_helper
+DEBUG - 2012-09-07 11:55:34 --> Helper loaded: cookie_helper
+DEBUG - 2012-09-07 11:55:34 --> Helper loaded: regions_helper
+DEBUG - 2012-09-07 11:55:34 --> Language file loaded: language/english/rif_cs_lang.php
+DEBUG - 2012-09-07 11:55:34 --> Database Driver Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> cURL Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> Controller Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> Model Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> Model Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> Final output sent to browser
+DEBUG - 2012-09-07 11:55:34 --> Total execution time: 0.1604
+DEBUG - 2012-09-07 11:55:34 --> Config Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> Hooks Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> Utf8 Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> UTF-8 Support Enabled
+DEBUG - 2012-09-07 11:55:34 --> URI Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> Router Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> Output Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> Security Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> Input Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> Global POST and COOKIE data sanitized
+DEBUG - 2012-09-07 11:55:34 --> Language Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> Loader Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> Helper loaded: url_helper
+DEBUG - 2012-09-07 11:55:34 --> Helper loaded: form_helper
+DEBUG - 2012-09-07 11:55:34 --> Helper loaded: cookie_helper
+DEBUG - 2012-09-07 11:55:34 --> Helper loaded: regions_helper
+DEBUG - 2012-09-07 11:55:34 --> Language file loaded: language/english/rif_cs_lang.php
+DEBUG - 2012-09-07 11:55:34 --> Database Driver Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> cURL Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> Controller Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> Model Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> Model Class Initialized
+DEBUG - 2012-09-07 11:55:34 --> Final output sent to browser
+DEBUG - 2012-09-07 11:55:34 --> Total execution time: 0.1664
+DEBUG - 2012-09-07 11:56:02 --> Config Class Initialized
+DEBUG - 2012-09-07 11:56:02 --> Hooks Class Initialized
+DEBUG - 2012-09-07 11:56:02 --> Utf8 Class Initialized
+DEBUG - 2012-09-07 11:56:02 --> UTF-8 Support Enabled
+DEBUG - 2012-09-07 11:56:02 --> URI Class Initialized
+DEBUG - 2012-09-07 11:56:02 --> Router Class Initialized
+DEBUG - 2012-09-07 11:56:02 --> Output Class Initialized
+DEBUG - 2012-09-07 11:56:02 --> Security Class Initialized
+DEBUG - 2012-09-07 11:56:02 --> Input Class Initialized
+DEBUG - 2012-09-07 11:56:02 --> Global POST and COOKIE data sanitized
+DEBUG - 2012-09-07 11:56:02 --> Language Class Initialized
+DEBUG - 2012-09-07 11:56:02 --> Loader Class Initialized
+DEBUG - 2012-09-07 11:56:02 --> Helper loaded: url_helper
+DEBUG - 2012-09-07 11:56:02 --> Helper loaded: form_helper
+DEBUG - 2012-09-07 11:56:02 --> Helper loaded: cookie_helper
+DEBUG - 2012-09-07 11:56:02 --> Helper loaded: regions_helper
+DEBUG - 2012-09-07 11:56:02 --> Language file loaded: language/english/rif_cs_lang.php
+DEBUG - 2012-09-07 11:56:02 --> Database Driver Class Initialized
+DEBUG - 2012-09-07 11:56:03 --> cURL Class Initialized
+DEBUG - 2012-09-07 11:56:03 --> Controller Class Initialized
+DEBUG - 2012-09-07 11:56:03 --> Model Class Initialized
+DEBUG - 2012-09-07 11:56:03 --> Model Class Initialized
+DEBUG - 2012-09-07 11:56:03 --> Final output sent to browser
+DEBUG - 2012-09-07 11:56:03 --> Total execution time: 0.1263
+DEBUG - 2012-09-07 11:56:03 --> Config Class Initialized
+DEBUG - 2012-09-07 11:56:03 --> Hooks Class Initialized
+DEBUG - 2012-09-07 11:56:03 --> Utf8 Class Initialized
+DEBUG - 2012-09-07 11:56:03 --> UTF-8 Support Enabled
+DEBUG - 2012-09-07 11:56:03 --> URI Class Initialized
+DEBUG - 2012-09-07 11:56:03 --> Router Class Initialized
+DEBUG - 2012-09-07 11:56:03 --> Output Class Initialized
+DEBUG - 2012-09-07 11:56:03 --> Security Class Initialized
+DEBUG - 2012-09-07 11:56:03 --> Input Class Initialized
+DEBUG - 2012-09-07 11:56:03 --> Global POST and COOKIE data sanitized
+DEBUG - 2012-09-07 11:56:03 --> Language Class Initialized
+DEBUG - 2012-09-07 11:56:03 --> Loader Class Initialized
+DEBUG - 2012-09-07 11:56:03 --> Helper loaded: url_helper
+DEBUG - 2012-09-07 11:56:03 --> Helper loaded: form_helper
+DEBUG - 2012-09-07 11:56:03 --> Helper loaded: cookie_helper
+DEBUG - 2012-09-07 11:56:03 --> Helper loaded: regions_helper
+DEBUG - 2012-09-07 11:56:03 --> Language file loaded: language/english/rif_cs_lang.php
+DEBUG - 2012-09-07 11:56:03 --> Database Driver Class Initialized
+DEBUG - 2012-09-07 11:56:03 --> cURL Class Initialized
+DEBUG - 2012-09-07 11:56:03 --> Controller Class Initialized
+DEBUG - 2012-09-07 11:56:03 --> Model Class Initialized
+DEBUG - 2012-09-07 11:56:03 --> Model Class Initialized
+DEBUG - 2012-09-07 11:56:03 --> Final output sent to browser
+DEBUG - 2012-09-07 11:56:03 --> Total execution time: 0.1398
+DEBUG - 2012-09-07 11:56:03 --> Config Class Initialized
+DEBUG - 2012-09-07 11:56:03 --> Hooks Class Initialized
+DEBUG - 2012-09-07 11:56:03 --> Utf8 Class Initialized
+DEBUG - 2012-09-07 11:56:03 --> UTF-8 Support Enabled
+DEBUG - 2012-09-07 11:56:03 --> URI Class Initialized
+DEBUG - 2012-09-07 11:56:03 --> Router Class Initialized
+DEBUG - 2012-09-07 11:56:03 --> Output Class Initialized
+DEBUG - 2012-09-07 11:56:03 --> Security Class Initialized
+DEBUG - 2012-09-07 11:56:03 --> Input Class Initialized
+DEBUG - 2012-09-07 11:56:03 --> Global POST and COOKIE data sanitized
+DEBUG - 2012-09-07 11:56:03 --> Language Class Initialized
+DEBUG - 2012-09-07 11:56:03 --> Loader Class Initialized
+DEBUG - 2012-09-07 11:56:03 --> Helper loaded: url_helper
+DEBUG - 2012-09-07 11:56:03 --> Helper loaded: form_helper
+DEBUG - 2012-09-07 11:56:03 --> Helper loaded: cookie_helper
+DEBUG - 2012-09-07 11:56:03 --> Helper loaded: regions_helper
+DEBUG - 2012-09-07 11:56:03 --> Language file loaded: language/english/rif_cs_lang.php
+DEBUG - 2012-09-07 11:56:03 --> Database Driver Class Initialized
+DEBUG - 2012-09-07 11:56:03 --> cURL Class Initialized
+DEBUG - 2012-09-07 11:56:03 --> Controller Class Initialized
+DEBUG - 2012-09-07 11:56:03 --> Model Class Initialized
+DEBUG - 2012-09-07 11:56:03 --> Model Class Initialized
+DEBUG - 2012-09-07 11:56:03 --> Final output sent to browser
+DEBUG - 2012-09-07 11:56:03 --> Total execution time: 0.1372
