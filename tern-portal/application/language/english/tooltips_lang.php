f19577e70480785077668e58be545eb2755e225c
Yi Sun <sunyi000@gmail.com>
2012-09-25 14:56:12 +1000

    tern-portal/application/language/english/tooltips_lang.php

---
+++ tern-portal/application/language/english/tooltips_lang.php
@@ -0,0 +1,89 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+$lang['term'] = "Enter search term here";
+$lang['term_boolean'] = "Select boolean operator";
+$lang['termporal'] = "select temporal coverage range ";
+$lang['region'] = "Select different regions";
+$lang['for'] = "add field of research ";
+$lang['facility'] = "select facilities to refine search results";
+
+$lang['term_helptitle']="Advance boolean search help";
+$lang['term_helptext']="<p>
+                            The Boolean operators of AND, OR and NOT enable refinement or extension of a search result set.
+                        </p>
+                        <p>
+                            Radio buttons enable the selection of one operator at a time.  To add multiple terms to a search strategy select the <b>Search</b> button in-between each addition. 
+                        </p>
+                        <p>
+                            To narrow a search use the <b>Include (AND)</b> operator
+                            e.g.  Vegetation AND Fire locates records that must include both terms.
+                        </p>
+                        <p>
+                            To widen a search use <b>Expand (OR)</b> operator 
+                            e.g. Vegetation OR Fire locates records that can either include the term Vegetation or the term Fire or both terms.
+                        </p>
+                        <p>
+                            To remove terms from a search use the Exclude (NOT) operator 
+                            e.g. Vegetation NOT Fire locates records that include the term vegetation but not any records that include the term Fire. 
+                        </p>
+                        <p>
+                            Terms within quotes are treated as a phrase
+                            e.g.”Kangaroo Island” AND Fire locates records that include the term fire within a record containing the phrase Kangaroo Island. 
+
+                        </p>
+                        <p>
+                            To remove a search term, delete the term from the Current Search box.
+                        </p>
+                    ";
+
+
+$lang['facility_helptitle']="Facility facet help";
+$lang['facility_helptext'] ="<p>
+                                Each TERN Facility works in one or more ecosystem science domains.   Use the checkboxes to restrict your search to records produced by one or more specific facilities.   The record count next to each facility name represents the number of records available which match your search parameters. 
+                             </p>
+                             <p>
+                                To remove a facility restriction, delete the parameter from the Current Search box.
+                             </p>
+                    ";
+
+$lang['for_helptitle']="Field of Research help";
+$lang['for_helptext'] ="<p>
+                            Fields of Research (FoR) codes assigned to the current result set are displayed in a hierarchical tree structure.   Top level fields are displayed initially, those with a child node change colour on hover.  Click on a top level field to display the child node. 
+                        </p>
+                        <p>
+                            Use the checkboxes to limit your search to one or more FoRs, update the search using the Go button.  
+                        </p>
+                        <p>
+                            The record count relates to the number of FoR matches within the displayed result set, records may have one or more FoR assigned
+                        </p>
+                        <p>
+                            To remove a FoR restriction, delete the parameter from the Current Search box.
+                        </p>
+                        <p>
+                            Fields of Research (FoR) have been selected using the <a href=\"http://www.abs.gov.au/ausstats/abs@.nsf/mf/1297.0\">Australian and New Zealand Standard Research Classification (ANZSRC)</a>
+                        </p>
+                        
+                    ";
+
+$lang['region_helptitle']="Regions facet help";
+$lang['region_helptext'] ="<p>
+                                Select a Region Type from the drop-down list to display region names.  Boundaries for the chosen type will appear on the map – the map is not interactive.  
+                           </p>
+                           <p>
+                                Select a region name from the list displayed to restrict search results to those records with a spatial coverage intersecting with the selected region. The map displayed will load at the Australian continental scale with the selected region displayed as a grey boundary; pins will be visible for matching records in the result set.                           
+                           </p>
+                           <p>
+                                Only one region can be searched at any one time. Records without spatial coverage data will not be displayed in this result set.                             
+                           </p>
+                           <p>
+                                To remove a Region, delete the parameter from the Current Search box.                           
+                           </p>
+";
+?>
+
