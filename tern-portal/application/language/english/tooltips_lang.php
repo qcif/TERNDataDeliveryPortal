d41ce6cd73b98de6b070137ab91afcddc791b4f1
Yi Sun <sunyi000@gmail.com>
2012-09-25 15:13:54 +1000

    help content changes

--- tern-portal/application/language/english/tooltips_lang.php
+++ tern-portal/application/language/english/tooltips_lang.php
@@ -15,21 +15,18 @@ $lang['facility'] = "select facilities to refine search results";
 
 $lang['term_helptitle']="Advance boolean search help";
 $lang['term_helptext']="<p>
-                            The Boolean operators of AND, OR and NOT enable refinement or extension of a search result set.
+                            Use AND, OR and NOT to refine your search results.
                         </p>
                         <p>
-                            Radio buttons enable the selection of one operator at a time.  To add multiple terms to a search strategy select the <b>Search</b> button in-between each addition. 
-                        </p>
-                        <p>
-                            To narrow a search use the <b>Include (AND)</b> operator
+                            To narrow a search use the <b>AND</b> operator
                             e.g.  Vegetation AND Fire locates records that must include both terms.
                         </p>
                         <p>
-                            To widen a search use <b>Expand (OR)</b> operator 
+                            To widen a search use the <b>OR</b> operator 
                             e.g. Vegetation OR Fire locates records that can either include the term Vegetation or the term Fire or both terms.
                         </p>
                         <p>
-                            To remove terms from a search use the Exclude (NOT) operator 
+                            To remove terms from a search use the <b>NOT</b> operator 
                             e.g. Vegetation NOT Fire locates records that include the term vegetation but not any records that include the term Fire. 
                         </p>
                         <p>
@@ -38,51 +35,51 @@ $lang['term_helptext']="<p>
 
                         </p>
                         <p>
-                            To remove a search term, delete the term from the Current Search box.
+                            To remove a search term, delete the term from the <i>Current Search</i> box.
                         </p>
                     ";
 
 
 $lang['facility_helptitle']="Facility facet help";
 $lang['facility_helptext'] ="<p>
-                                Each TERN Facility works in one or more ecosystem science domains.   Use the checkboxes to restrict your search to records produced by one or more specific facilities.   The record count next to each facility name represents the number of records available which match your search parameters. 
+                                Each TERN Facility works in one or more ecosystem science domains.   Use the checkboxes next to each Facility to restrict your search to records produced by that facility.  More than one facility may be selected.  The number next to each facility name represents the number of records available which match your search parameters. 
                              </p>
                              <p>
-                                To remove a facility restriction, delete the parameter from the Current Search box.
+                                To remove a facility restriction, delete the parameter from the <i>Current Search</i> box.
                              </p>
                     ";
 
 $lang['for_helptitle']="Field of Research help";
 $lang['for_helptext'] ="<p>
-                            Fields of Research (FoR) codes assigned to the current result set are displayed in a hierarchical tree structure.   Top level fields are displayed initially, those with a child node change colour on hover.  Click on a top level field to display the child node. 
+                            Fields of Research (FoR) have been selected using the <a href=\"http://www.abs.gov.au/ausstats/abs@.nsf/mf/1297.0\">Australian and New Zealand Standard Research Classification (ANZSRC)</a>
+                        </p>
+                        <p>
+                            Fields of Research (FoR) codes assigned to the current result set are displayed in a hierarchical tree structure.   Top level or primary fields are displayed initially, those with a secondary field change colour on hover.  Click on a primary field to display the secondary level fields.   
                         </p>
                         <p>
                             Use the checkboxes to limit your search to one or more FoRs, update the search using the Go button.  
                         </p>
                         <p>
-                            The record count relates to the number of FoR matches within the displayed result set, records may have one or more FoR assigned
+                            The number next to each field relates to the number of matches within the displayed result set, records may have one or more FoR assigned.
                         </p>
                         <p>
-                            To remove a FoR restriction, delete the parameter from the Current Search box.
-                        </p>
-                        <p>
-                            Fields of Research (FoR) have been selected using the <a href=\"http://www.abs.gov.au/ausstats/abs@.nsf/mf/1297.0\">Australian and New Zealand Standard Research Classification (ANZSRC)</a>
+                            To remove a FoR restriction, delete the parameter from the <i>Current Search</i> box.
                         </p>
                         
                     ";
 
 $lang['region_helptitle']="Regions facet help";
 $lang['region_helptext'] ="<p>
-                                Select a Region Type from the drop-down list to display region names.  Boundaries for the chosen type will appear on the map – the map is not interactive.  
+                                Select a <b>Region Type</b> from the drop-down list to display region names.  Boundaries for the chosen type will appear on the map – the map is not interactive.  
                            </p>
                            <p>
-                                Select a region name from the list displayed to restrict search results to those records with a spatial coverage intersecting with the selected region. The map displayed will load at the Australian continental scale with the selected region displayed as a grey boundary; pins will be visible for matching records in the result set.                           
+                                Select a region name from the list displayed to restrict search results to those records with a spatial coverage intersecting with the selected region. The map displayed will load at the Australian continental scale with the selected region displayed as a grey boundary; pins will be visible for matching records in the result set
                            </p>
                            <p>
-                                Only one region can be searched at any one time. Records without spatial coverage data will not be displayed in this result set.                             
+                               Only one region can be searched at any one time. Records without spatial coverage data will not be displayed in this result set.  
                            </p>
                            <p>
-                                To remove a Region, delete the parameter from the Current Search box.                           
+                                To remove a Region, delete the parameter from the <i>Current Search</i> box.                           
                            </p>
 ";
 ?>
