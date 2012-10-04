288770d10a2eaa410c7c96aa25074c68d39f42e3
Dewi Wahyuni <dewi.wahyuni@qut.edu.au>
2012-10-04 16:32:00 +1000

    New marker images

--- tern-portal/application/views/content/mapproto_service.php
+++ tern-portal/application/views/content/mapproto_service.php
@@ -5,7 +5,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">  
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
     <head>   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/> 
-        <link type="text/css" href="<?php echo base_url(); ?>css/tern.css" rel="stylesheet" />
+        <link type="text/css" href="<?php echo base_url(); ?>css/main.css" rel="stylesheet" />
         <link type="text/css" href="<?php echo base_url(); ?>css/smoothness/jquery-ui-1.8.20.custom.css" rel="stylesheet" />
         <style >
           .olPopup { border : 0 !important;}
