2dea658b5faeab020e4a7c3d46a761cc175ac738
Dewi Wahyuni <dewi.wahyuni@qut.edu.au>
2012-10-17 10:44:37 +1000

    Access Submit data changes

--- tern-portal/application/views/terndata/accessdata.php
+++ tern-portal/application/views/terndata/accessdata.php
@@ -6,7 +6,7 @@
         <div class="content">
             <ul>
                <li>                  
-                    <a href="#Auscover">Auscover</a>
+                    <a href="#AusCover">AusCover</a>
                 </li>      
                 <li>                  
                     <a href="#Coasts">Australian Coastal Ecosystem</a>
@@ -18,7 +18,7 @@
                     <a href="#Soils">Soil and Landscape Grid of Australia</a>
                 </li>  
                 <li>                  
-                    <a href="#MSPN">Multi Scale Plot Network</a>
+                    <a href="#MSPN">Multi-Scale Plot Network</a>
                 </li>      
                 <li>                  
                     <a href="#ASN">Australian Supersites Network </a>
@@ -37,7 +37,7 @@
 <div id="staticContentWhite">
     <h1 class="margin10">Access Data</h1>
     <p>TERN provides access to wide variety of ecosystem data covering multiple disciplines through several “Facility” based  data delivery systems.</p><p>The information below explains the delivery status, and how to access discipline specific datasets from each of the TERN Facilities.</p>
-    <h2><a name="Auscover"></a>Auscover</h2>
+    <h2><a name="AusCover"></a>AusCover</h2>
     <p>AusCover provides satellite-based biophysical map products, remote sensing data time-series, and selected high-spatial resolution datasets. The data delivery system includes a network of nationally distributed computing infrastructure, each with a suite of web-enabled data services (THREDDS, OPeNDAP, Geoserver, etc.). The <a href="http://data.auscover.org.au/" target="_blank" class="external">AusCover data website</a> links to all the central and distributed servers and services along with documentation about the AusCover data system. The <a href="http://data.auscover.org.au/geonetwork" target="_blank" class="external">AusCover metadata catalog service</a> provides metadata information about the data products available from the Facility. All gridded products will be translated to Network Common Data Format – Climate and Forecast (NetCDF-CF) to make them available for browsing, subsetting or aggregation via web services. The AusCover visualisation portal is under development and is due for release in first quarter of 2013.</p>   
     <h2><a name="Coasts"></a>Australian Coastal Ecosystem</h2>
     <p>Data from the Australian Coastal Ecosystems Facility (ACEF) will be discoverable through the <a href="http://portal-tern.org.au">TERN Data Discovery Portal</a> and <a href="http://portal.aodn.org.au/webportal/" target="_blank" class="external">the Australian Ocean Data Network (AODN)</a>. The delivery system and visualisation services of the ACEF are under development and will be available from November 2012. </p>
@@ -50,16 +50,16 @@ At present, access to the data requires registering for an account from the port
       <h2><a name="Soils"></a>Soil and Landscape Grid of Australia</h2>
          TERN’s Soil and Landscape Facility will provide a number of new national soil attribute and terrain derivative data sets, primarily as fine resolution raster/grid files. As data products become available they will be made viewable through the TERN Soil Data Portal, with links to meta-data providing information regarding access and licensing. Data sets will be made available for download where possible, through the <a href="http://www.asris.csiro.au/" target="_blank" class="external">Australian Soil Resource Information System (ASRIS)</a> and the <a href="https://data.csiro.au/dap" target="_blank" class="external">CSIRO Data Access Portal (DAP)</a>.
          
-     <h2><a name="MSPN"></a>Multi Scale Plot Network</h2>
-    <p>The Multi-Scale Plot Network (MSPN) will publish range of ecological plot based datasets covering rangelands, forests, long-term ecological monitoring programs and sub-continental transects that span from coasts to inland. All the data will be available via Australian Ecological Knowledge and Observation System (<a href="http://www.aekos.org.au/" target="_blank" class="external">ÆKOS</a>), developed by TERN Eco-informatics Facility. Australian Supersites Network (ASN), a sub-Facility of MSPN uses different data delivery mechanism due to the nature of the data collected by the sub-Facility. </p>
+     <h2><a name="MSPN"></a>Multi-Scale Plot Network</h2>
+    <p>The Multi-Scale Plot Network (MSPN) will publish range of ecological plot based datasets covering rangelands, forests, long-term ecological monitoring programs and sub-continental transects that span from coasts to inland. All the data will be available via the Australian Ecological Knowledge and Observation System (<a href="http://www.aekos.org.au/" target="_blank" class="external">ÆKOS</a>), developed by TERN Eco-informatics Facility. Australian Supersites Network (ASN), a sub-Facility of MSPN uses a different data delivery mechanism due to the nature of the data collected by the sub-Facility. </p>
      <h2><a name="ASN"></a>Australian Supersites Network </h2>
     <p>The Australian Supersites Network (ASN) is a sub-Facility in the Multi-Scale Plot Network and publishes datasets about Australian Terrestrial biomes from 10 different supersites across Australia. Data sets uploaded to the ASN are stored in a secure Metacat repository. Access to the data is through the <a href="http://www.tern-supersites.net.au/" target="_blank" class="external">ASN web portal</a>.
 Some of the data sets from the portal may be restricted and requires login credentials. Requests for access and login details can be made by clicking on the record you require or by contacting <a href="mailto:info%40tern-supersites.net.au">Siobhann McCafferty</a></p>
 
   <h2><a name="Eco-informatics"></a>Eco-informatics</h2>
-    <p>Eco-Informatics Facility has developed Australian Ecological Knowledge and Observation System (<a href="http://www.aekos.org.au/" target="_blank" class="external">ÆKOS</a>) to store rich contextual ecological data and is accessed via a sophisticated online <a href="http://www.aekos.org.au/access_portal" target="_blank" class="external">data portal</a>. The Portal enables browsing, searching, viewing and retrieval of many different types of ecological data systematically collected at the sampling unit level (plot, quadrat, transect, trapping array, tissue sample). 
+    <p>The Eco-Informatics Facility has developed Australian Ecological Knowledge and Observation System (<a href="http://www.aekos.org.au/" target="_blank" class="external">ÆKOS</a>) to store rich contextual ecological data and is accessed via a sophisticated online <a href="http://www.aekos.org.au/access_portal" target="_blank" class="external">data portal</a>. The Portal enables browsing, searching, viewing and retrieval of many different types of ecological data systematically collected at the sampling unit level (plot, quadrat, transect, trapping array, tissue sample). 
 Datasets can be referenced in ÆKOS with a metadata description only, or a metadata description with an attached opaque file such as an Excel spreadsheet, or the data within a significant dataset can be fully integrated and federated, with contextual information describing the data added so that researchers can assess its fitness for purpose and reuse. 
-Fully ingested and integrated data is accessible at the sampling unit level (e.g., number per quadrat) and this enables selected records from many datasets to be combined and downloaded via a shopping cart. Data are free and openly accessible under Creative Commons v3.0 Australia– Attribution or Share Alike licences, or the TERN Attribution Data Licence v1.0. Restricted data (<1% of datasets) are not currently published and remain with data custodians.
+Fully ingested and integrated data is accessible at the sampling unit level (e.g., number per quadrat) and this enables selected records from many datasets to be combined and downloaded via a shopping cart. Data are free and openly accessible under Creative Commons v3.0 Australia– Attribution or Share Alike licences.  Some data (<1% of datasets) are subject to the TERN Attribution Data Licence v1.0. Restricted data so are not currently published and remain with data custodians.
 </p>
 
 <h2><a name="EMAST"></a>Ecosystem Modelling and Scaling Infrastructure</h2>
