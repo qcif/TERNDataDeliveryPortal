b6d37e40584e83de4d8e5e3855faa591249a02a3
Yi Sun <sunyi000@gmail.com>
2012-10-25 14:39:28 +1000

    term of use change

--- tern-portal/application/views/terndata/terms.php
+++ tern-portal/application/views/terndata/terms.php
@@ -4,17 +4,16 @@
 <div id="staticContentWhite">
     <h1 class="margin10">Terms of use</h1>
  
-<p>By using TERN Data Discovery Portal contents you are agreeing to be bound by these Terms of Use.</p>
-
-<p>Content provided through this portal may be subjected to specific terms and conditions imposed by a data provider and the data provider may include certain licenses to use the Content.</p>
+<p>By using the TERN Data Discovery Portal you are agreeing to be bound by these Terms of Use.  These terms may change from time to time.</p>
 
 <h2>Copyright</h2>
 
-<p>Copyright or other rights in material included within the TERN Data Discovery Portal may belong to Terrestrial Ecosystem Research Network (TERN) at University of Queensland or third parties. Content contained within this portal is accessible under licenses specified by data providers. Please refer to specific licences associated with specific content for relevant terms of use.</p>
-
+<p>Copyright or other rights in material included within the TERN Data Discovery Portal may belong to Terrestrial Ecosystem Research Network (TERN) at University of Queensland or third parties.  Logos are used with consent.</p>
+<p>Content contained within this portal is accessible under licenses specified by data providers. Please refer to specific licences associated with specific content for relevant terms of use.</p>
 <p>Requests for further authorisation should be directed via our <a href="/contact">contact page</a>.</p>
+
 <h2>Disclaimer </h2>
-<p>By accessing content through TERN Data Discovery Portal, you accept all risks and responsibility for losses, damages, costs and other consequences (direct or indirect) resulting directly or indirectly from using this site or portal and any information or material available from it.</p>
+<p>The TERN Data Discovery Portal and its content is provided on an "as is" and "as available" basis. You understand and agree that you use the TERN Data Discovery Portal at your own discretion and risk and that you will be solely responsible for any damage or loss whatsoever that results from such use including use of any data obtained through the TERN Data Discovery Portal.</p>
 
 <p>Web links to this site from external, third party websites should not be constructed as implying any relationships with and/or endorsement of the external site or its content by TERN.</p>
 
@@ -23,7 +22,7 @@
 <h2>Acknowledgements</h2>
 <p>The TERN Data Discovery Portal utilises resources from the following third-party providers.</p>
 <h3>Mapping data</h3>
-<p>The region maps used in the search interface has been provided by the following agencies:</p>
+<p>The region maps used in the search interface have been provided by the following agencies:</p>
 <ul style="line-height: 30px;">
   <li><a href="http://www.environment.gov.au/parks/nrs/science/bioregion-framework/ibra/index.html" class="external" target="_blank">Interim Biogeographic Regionalisation of Australia (v7)</a>: Australian Government - Department of Sustainability, Environment, Water, Population and Communities</li>
   <li><a href="http://www.environment.gov.au/metadataexplorer/full_metadata.jsp?docId=%7BFA68F769-550B-4605-A0D5-50B10ECD0EB9%7D" class="external" target="_blank">Natural Resource Management (NRM) Regions (2010)</a>: Australian Government - Department of Sustainability, Environment, Water, Population and Communities</li>
@@ -31,6 +30,6 @@
   <li><a href="http://www.abs.gov.au/AUSSTATS/abs@.nsf/Lookup/1259.0.30.001Main+Features1July%202011?OpenDocument" class="external" target="_blank">Australian Standard Geographical Classification (ASGC) Digital Boundaries, Australia, July 2011</a>: Australian Government - Australian Bureau of Statistics</li>
   </ul>
 <br/>
-<p>Please note that the links provided above are current as of October 1, 2012. In no way do these data providers endorse the TERN Data Discovery Portal.</p>
+<p>Please note that the links provided above are current as of October 1, 2012 and are for information only.</p>
 </div>
 <?php $this->load->view('tpl/footer');?>
