bffb1e090e38d91ade850f1a45b6a335f831d83d
Dewi Wahyuni <dewi.wahyuni@qut.edu.au>
2012-11-21 13:09:10 +1000

    Ipad WMS click

--- tern-portal/js/MapWidget.js
+++ tern-portal/js/MapWidget.js
@@ -357,11 +357,40 @@ function MapWidget(mapId, overviewMap, options){
 
 MapWidget.prototype.registerClickRegions = function(layers,callback){
     var obj = this;
+    OpenLayers.Control.Click = OpenLayers.Class(OpenLayers.Control, {
+  defaultHandlerOptions: {
+    'single': true,
+    'double': false,
+    'pixelTolerance': 0,
+    'stopSingle': false,
+    'stopDouble': false
+  },
+  initialize: function(options) {
+    this.handlerOptions = OpenLayers.Util.extend(
+      {}, this.defaultHandlerOptions
+    );
+    OpenLayers.Control.prototype.initialize.apply(
+      this, arguments
+    );
+    this.handler = new OpenLayers.Handler.Click(
+      this, {
+        'click': this.trigger
+      }, this.handlerOptions
+    );
+  }
+}); 
+   
+var click = new OpenLayers.Control.Click( { trigger: function(e) {
+     obj.handleMapClickRegions(e,layers,callback);   
+}});
+this.map.addControl(click);
+ click.activate();
+/*
     this.map.events.register('click', this.map, function(e){           
         obj.handleMapClickRegions(e,layers,callback);   
        
     });    
-     
+  */   
 }
 
 /*  ------------------------------------------------------------  
