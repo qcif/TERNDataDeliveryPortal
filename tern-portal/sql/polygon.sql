3008afa103addf72ad671f4e2135fa180e58146e
Dewi Wahyuni <dewi.wahyuni@qut.edu.au>
2012-12-20 13:48:30 +1000

    New polygon SQL

--- tern-portal/sql/polygon.sql
+++ tern-portal/sql/polygon.sql
@@ -82,18 +82,19 @@ DECLARE
 BEGIN
    inputpoly := dba.ST_GEOMFROMTEXT('POLYGON((' || coords || '))',4326);
 RETURN QUERY 
-      SELECT key FROM (SELECT distinct se.registry_object_key as key,dba.tern_coords_convert(CAST(sl.value as VARCHAR)) as geom
- FROM dba.tbl_spatial_extents se 
-INNER JOIN dba.tbl_spatial_locations sl ON sl.spatial_location_id = se.spatial_location_id
- WHERE (sl.type = 'kmlPolyCoords' OR sl.type='gmlKmlPolyCoords') AND se.bound_box &&  box ((point($1,$4)),(point($3,$2)))) e WHERE 
-     dba.ST_INTERSECTS(geom,inputpoly) IS TRUE UNION
-  SELECT key FROM (
-   SELECT se.registry_object_key as key, dba.tern_box_convert(CAST(se.bound_box as VARCHAR)) as geom 
- FROM dba.tbl_spatial_extents se 
-INNER JOIN dba.tbl_spatial_locations sl ON sl.spatial_location_id = se.spatial_location_id
- WHERE sl.type = 'iso19139dcmiBox' AND se.bound_box &&  box ((point($1,$4)),(point($3,$2)))
- ) f WHERE dba.ST_INTERSECTS(geom,inputpoly) IS TRUE ;
-  
+      SELECT key FROM (
+               SELECT distinct se.registry_object_key as key,dba.tern_coords_convert(CAST(sl.value as VARCHAR)) as geom
+               FROM dba.tbl_spatial_extents se 
+               INNER JOIN dba.tbl_spatial_locations sl ON sl.spatial_location_id = se.spatial_location_id
+               WHERE (sl.type = 'kmlPolyCoords' OR sl.type='gmlKmlPolyCoords') AND se.bound_box &&  box ((point($1,$4)),(point($3,$2)))
+      )e WHERE dba.ST_INTERSECTS(geom,inputpoly) IS TRUE 
+     UNION
+     SELECT key FROM (
+               SELECT se.registry_object_key as key, dba.tern_box_convert(CAST(se.bound_box as VARCHAR)) as geom 
+               FROM dba.tbl_spatial_extents se 
+               INNER JOIN dba.tbl_spatial_locations sl ON sl.spatial_location_id = se.spatial_location_id
+               WHERE sl.type = 'iso19139dcmiBox' AND se.bound_box &&  box ((point($1,$4)),(point($3,$2)))
+     )f WHERE dba.ST_INTERSECTS(geom,inputpoly) IS TRUE ;  
  END;
  
  $_$
